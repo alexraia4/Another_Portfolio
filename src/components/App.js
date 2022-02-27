334591b510db026062192af37b39faca880d1ee1
DerpHead <alexraia4@gmail.com>
2022-02-27 14:52:15 -0500

    Removed bloat and set up initial components.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Introduction from './Introduction'
+import '../styles/reset.css';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <Introduction />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Introduction.js
@@ -0,0 +1,11 @@
+import '../styles/Introduction.css';
+
+function Introduction() {
+  return (
+    <div className="Introduction">
+      Introduction Page
+    </div>
+  );
+}
+
+export default Introduction;
