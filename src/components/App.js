ebf6860975430c44499e48539c8d4551e199a726
DerpHead <alexraia4@gmail.com>
2022-03-03 19:53:47 -0500

    Basically scraped everything and made a new site. Optimizing for mobile first on introduction page.

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,11 @@
-import GradientBar from './gradientBar';
 import Introduction from './Introduction'
-import Projects from './Projects'
 import '../styles/reset.css';
 import '../styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <GradientBar />
       <Introduction />
-      <Projects />
-      <GradientBar />
     </div>
   );
 }
--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,34 +1,28 @@
 import '../styles/Introduction.css';
-import Me from '../images/me.jpg'
+import Me from '../images/me.jpg';
+import LinkedInLogo from '../images/linkedIn.png';
+import gitHubLogo from '../images/gitHub.png'
+import envelope from '../images/envelope.png'
 
 function Introduction() {
-   return (
-      <div className="Introduction">
-        <div className='Introduction_me_card'>
-          <p className='Introduction_me_card_title'>
-            Alexander Raia
-          </p>
-          <img src={Me} width="300" height="300" className='Introduction_me_card_image'/>
-          <p>Web Developer</p>
-          <p>&amp;</p>
-          <p>Software Engineer</p>
+  return (
+    <div className="Introduction">
+      <div className="Introduction_bar">
+        <img src={Me} alt="This is me" className="Introduction_bar_image"/>
+        <h1 className='Introduction_bar_name'>Alexander Raia</h1>
+        <div className='Introduction_bar_title'>
+          <h2 >Web Developer</h2>
+          <h2 className='Introduction_bar_amp'>&amp;</h2>
+          <h2>Software Engineer</h2> 
         </div>
-        <div className='Introduction_container'>
-          <div className='Introduction_links_container'>
-            <a href="https://docs.google.com/document/d/1o2CK0zm48t1kXBYujEKx1a52jLxJ2-0F2iRdn_M3xkY/edit?usp=sharing" className="Introduction_text_link">Resumé</a>
-            <a href="https://www.linkedin.com/in/alexander-raia/" className="Introduction_text_link">LinkedIn</a>
-            <a href="https://github.com/alexraia4" className="Introduction_text_link">GitHub</a>
-            
-          </div>
-          <p className='Introduction_text'>
-            Im a Full-Stack Software Engineer with experience working on all parts of a production application. I just left my last position and am looking for my next adventure. 
-          </p>
-          <p className='Introduction_text'>
-           Scroll Down for some more information.
-          </p>
+        <a href="https://docs.google.com/document/d/1o2CK0zm48t1kXBYujEKx1a52jLxJ2-0F2iRdn_M3xkY/edit?usp=sharing" className="Introduction_bar_resume">Resumé</a>
+        <div className='Introduction_bar_logos'>
+          <a href="https://www.linkedin.com/in/alexander-raia/"><img src={LinkedInLogo} alt="LinkedIn" className='Introduction_bar_logos_logo' /></a>
+          <a href="https://github.com/alexraia4" ><img src={gitHubLogo} alt="GitHub" className='Introduction_bar_logos_logo' /></a>
+          <a href="https://gmail.com" ><img src={envelope} alt="Email" className='Introduction_bar_logos_logo' /></a>
         </div>
-         
       </div>
+    </div>
   );
 }
 
